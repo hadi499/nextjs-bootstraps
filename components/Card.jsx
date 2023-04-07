c185a95587386f8678bf5f716ae1fa98d9480981
hadi499 <hadi.ygcl@gmail.com>
2023-04-07 14:27:28 +0700

    first

---
+++ components/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card mt-4 mx-auto" style={{ width: "28rem" }}>
+      <img
+        src="https://images.unsplash.com/photo-1480926965639-9b5f63a0817b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG9jZWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=400&q=60"
+        className="card-img-top"
+        height={270}
+        alt="..."
+      />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
