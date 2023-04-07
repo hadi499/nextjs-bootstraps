939e226d1d681ee8402f79c6a64b12d2f3ed76ce
hadi499 <hadi.ygcl@gmail.com>
2023-04-07 15:17:22 +0700

    truncate

--- components/Card.jsx
+++ components/Card.jsx
@@ -11,7 +11,7 @@ const Card = () => {
       />
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">
+        <p className="card-text text-truncate">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
